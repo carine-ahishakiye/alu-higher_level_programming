608ceec8e049189d0aa621ca478d8718caedbc22
carine-ahishakiye <c.yibukabay@alustudent.com>
2024-02-24 07:54:13 -0800

    3-print_alphabt.py

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-
-for i in range(97, 123):
-    if i in [102, 113]:
-        continue 
-    print("{}".format (chr(i)), end='')
+for i in range (97, 123):
+    if i in (101, 113):
+        continue
+    print ("{}".format(chr(i)), end='')
