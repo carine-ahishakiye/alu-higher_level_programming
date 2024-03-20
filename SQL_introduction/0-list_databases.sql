ed2baf35f77de55d2398a46287e15be39efcc854
carine-ahishakiye <c.yibukabay@alustudent.com>
2024-03-20 08:14:44 -0700

    0-list_databases.sql

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-# MySQL connection parameters
-HOST="localhost"
-USERNAME="root"
-PASSWORD="root"
+# This script lists all databases present on the MySQL server using a SQL script.
 
-# SQL script file
-SQL_SCRIPT="0-list_databases.sql"
+# MySQL connection parameters
+HOST="localhost"                # MySQL server hostname
+USERNAME="root"  # MySQL username
+PASSWORD="root"  # MySQL password
+
+# Path to SQL script file
+SQL_SCRIPT="/tmp/correction/5115582147978108665813637181410170299428_5/2128/6176/SQL_introduction/0-list_databases.sql"
 
 # Run the SQL script using mysql command-line client
 mysql -h $HOST -u $root -p$root < $SQL_SCRIPT
