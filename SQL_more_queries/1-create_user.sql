b6744ac93f9228d1fc8d1924b33122f71c1676bb
carine-ahishakiye <c.yibukabay@alustudent.com>
2024-03-22 03:38:16 -0700

    1-create_user.sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,5 @@
--- This script creates a MySQL server user with some privileges.
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
+-- creates a user and password
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+ALTER USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@localhost;
 FLUSH PRIVILEGES;
