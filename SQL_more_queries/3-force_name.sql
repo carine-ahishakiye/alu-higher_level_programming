18aa854a0ad8cd2a9972dad9455235da1e0b319b
carine-ahishakiye <c.yibukabay@alustudent.com>
2024-03-22 03:36:05 -0700

    3-force_name.sql

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,4 +1,4 @@
-ipt that creates the table force_name on MySQL server.
+-- script that creates the table force_name on MySQL server.
 CREATE TABLE IF NOT EXISTS force_name (
     id INT,
     name VARCHAR(256) NOT NULL
